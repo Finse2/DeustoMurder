2da58d9e364cd3d3a242a8aa17b1277b356263ba
Antal <antaljanos.nistal@opendeusto.es>
2026-09-08 08:58:09 +0200

    minor changes to Main

--- innerProy/src/model/Actor.java
+++ innerProy/src/model/Actor.java
@@ -2,11 +2,15 @@ package model;
 
 import ui.ActorsLook;
 
+import java.awt.*;
+
 public class Actor {
 
     final private String name;
+    final private Color color;
 
-    public Actor(String name) {
+    public Actor(String name, Color color) {
         this.name = name;
+        this.color = color;
     }
 }
