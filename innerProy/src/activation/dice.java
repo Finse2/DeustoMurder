dcb046467213bdb76923dfac8eec24938f5dc8d0
Luka Jacques-Paul Arevalo Douchet <lukajacquespaul.arev@opendeusto.es>
2026-09-14 19:08:54 +0200

    dice completion

--- innerProy/src/activation/dice.java
+++ innerProy/src/activation/dice.java
@@ -1,4 +1,24 @@
 package activation;
 
 public class dice {
+
+    static int min = 1;
+    static int max = 6;
+    private int roll_output;
+
+    public int getRoll_output() {
+        return roll_output;
+    }
+
+    public void setRoll_output(int roll_output) {
+        this.roll_output = roll_output;
+    }
+
+    public int roll() {
+        int roll1 = (int) (Math.random() * (max - min + 1) + min);
+        int roll2 = (int) (Math.random() * (max - min + 1) + min);
+        int roll = roll1 + roll2;
+        setRoll_output(roll);
+        return roll;
+    }
 }
